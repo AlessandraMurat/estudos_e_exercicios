7caf3b834f24374164af434b3016fade2b89c0a2
AlessandraMurat <seu-email@example.com>
2025-02-10 19:50:55 -0300

    Versão 2 do projeto do jogo secreto e resolução de desafio

---
+++ jogo_numero_secreto_v2/app.js
@@ -0,0 +1,9 @@
+let title = document.querySelector('h1');
+let paragraph = document.querySelector('.texto__paragrafo');
+
+title.innerHTML = 'Jogo so número secreto';
+paragraph.innerHTML= 'Escolha um número entre 1 e 10.'
+
+function verificarChute() {
+    
+}
