e0b7cb1bff0cea815e00aefe6eff76a129a41792
AlessandraMurat <seu-email@example.com>
2025-04-20 11:17:26 -0300

    Reorganização de projetos

---
+++ DNC/animacao_com_js/assets/script/script.js
@@ -0,0 +1,9 @@
+var copo = window.document.getElementById("copo");
+
+function AcenderLampada() {
+  copo.src = "assets/images/lampada_acesa.svg";
+}
+
+function ApagarLampada() {
+  copo.src = "assets/images/lampada_apagada.svg";
+}
